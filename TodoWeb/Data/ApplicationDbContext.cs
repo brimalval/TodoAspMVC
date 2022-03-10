c2dd000050ccf409a4994a8ad2fd53d26695ab51
Brian Valencia <qbmsvalencia@tip.edu.ph>
2022-03-10 14:16:27 +0800

    Setup for DB.

---
+++ TodoWeb/Data/ApplicationDbContext.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.EntityFrameworkCore;
+using TodoWeb.Models;
+
+namespace TodoWeb.Data
+{
+    public class ApplicationDbContext : DbContext
+    {
+        public ApplicationDbContext(DbContextOptions<ApplicationDbContext> options) : base(options)
+        {
+        }
+
+        public DbSet<Todo> Todos { get; set; }
+    }
+}
---
+++ TodoWeb/Models/Todo.cs
@@ -0,0 +1,17 @@
+﻿using System.ComponentModel;
+using System.ComponentModel.DataAnnotations;
+
+namespace TodoWeb.Models
+{
+    public class Todo
+    {
+        [Key]
+        public int Id { get; set; }
+        [Required]
+        public string Title { get; set; }
+        public string Description { get; set; } = "";
+        public bool Done { get; set; }
+        [DisplayName("Created at")]
+        public DateTime CreatedDateTime { get; set; } = DateTime.Now;
+    }
+}
