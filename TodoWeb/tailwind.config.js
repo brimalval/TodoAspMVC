d354d45db8848d020a9fc346a64b3d4b3836d7a7
Brian Valencia <qbmsvalencia@tip.edu.ph>
2022-03-15 04:21:20 +0800

    Restructured CSS

--- TodoWeb/tailwind.config.js
+++ TodoWeb/tailwind.config.js
@@ -1,8 +1,14 @@
+const defaultTheme = require('tailwindcss/defaultTheme');
+
 module.exports = {
   content: [
       "./Views/**/*.cshtml",
   ],
   theme: {
+    screens: {
+      'xs': '128px',
+      ...defaultTheme.screens,
+    },
     extend: {
       colors: {
         primary: '#AB47BC',
