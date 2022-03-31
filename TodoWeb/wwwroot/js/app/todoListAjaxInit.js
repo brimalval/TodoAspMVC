882f4b1646d9633a233b6fe280594205856ef377
Brian Valencia <qbmsvalencia@tip.edu.ph>
2022-03-31 17:26:42 +0800

    Moved common todo Ajax logic up to the generic submit function

--- TodoWeb/wwwroot/js/app/todoListAjaxInit.js
+++ TodoWeb/wwwroot/js/app/todoListAjaxInit.js
@@ -1,10 +1,19 @@
 ﻿(function (window) {
     function todoListAjaxInit() {
         // TODO : Ajax functions for todo tasks
-        function ajaxSubmit(form, type, successCallback, errorCallback) {
+        function ajaxSubmit(form, successCallback, errorCallback) {
+            console.log(errorCallback);
+            // Default form behaviors
+            successCallback ??= (data) => {
+                toastr.success(data.message);
+                refreshTodos(form);
+            }
+            errorCallback ??= (data) => {
+                showErrors(data.responseJSON.errors ?? []);
+            }
             var data = $(form).serialize();
             $.ajax({
-                type: type,
+                type: form.method,
                 url: form.action,
                 data: data,
                 success: successCallback,
@@ -39,42 +48,22 @@
             pageControl.submit();
         }
 
-        $(document).on('submit', '.todo-creation-form', function (e) {
-            const form = this;
-            const success = (data) => {
-                toastr.success("Task successfully created!");
-                refreshTodos(this);
-            }
-            const error = (data) => {
-                showErrors(data.responseJSON.errors);
-            }
-            ajaxSubmit(form, "POST", success, error);
+        $(".list-parent").on("submit", ".todo-creation-form", function (e) {
+            ajaxSubmit(this);
             return false;
         });
 
-        $(document).on('submit', '.delete-todo-form', function (e) {
-            const form = this;
-            const success = (data) => {
-                toastr.success("Task successfully deleted!");
-                refreshTodos(this);
-            }
-            const error = (data) => {
-                showErrors(data.responseJSON.errors);
-            }
+        $(".list-parent").on('submit', '.delete-todo-form', function (e) {
             if (confirm("Are you sure you want to delete this task?")) {
-                ajaxSubmit(form, "POST", success, error);
+                ajaxSubmit(this);
             }
             return false;
         });
 
-        $(document).on('submit', '.edit-todo-form', function (e) {
+        $(".list-parent").on('submit', '.edit-todo-form', function (e) {
             const form = this;
-            const success = (data) => {
-                console.log(data);
-                refreshTodos(this);
-            };
             const error = (data) => {
-                showErrors(data.responseJSON.errors);
+                showErrors(data.responseJSON.errors ?? []);
                 const form = this;
                 // Resetting input fields
                 $(form).find('input, textarea').each(function (e) {
@@ -82,46 +71,46 @@
                     inputField.value = inputField.defaultValue;
                 });
             };
-            ajaxSubmit(form, "POST", success, error);
+            ajaxSubmit(form, null, error);
             return false;
         });
 
-        $(document).on('change', '.edit-todo-form input, .edit-todo-form textarea', function (e) {
+        $(".list-parent").on('change', '.edit-todo-form input, .edit-todo-form textarea', function (e) {
             if (this.defaultValue != this.value.trim()) {
                 $(this).parents('form').submit();
             }
         });
 
-        $(document).on('change', '.edit-todo-form select', function (e) {
+        $(".list-parent").on('change', '.edit-todo-form select', function (e) {
             if (!this.options[this.selectedIndex].defaultSelected) {
                 $(this).parents('form').submit();
             }
         });
 
-        $(document).on('submit', '.page-control', function (e) {
+        $(".list-parent").on('submit', '.page-control', function (e) {
             const form = this;
             const id = form.dataset.id;
             const success = (data) => {
                 $('#todos-'+id).html(data);
             };
-            ajaxSubmit(form, "GET", success);
+            ajaxSubmit(form, success);
             return false;
         });
 
-        $(document).on('change', '.page-number', function(e) {
+        $(".list-parent").on('change', '.page-number', function(e) {
             if (this.value < 1) {
                 this.value = 1;
             }
             refreshTodos(this);
         });
 
-        $(document).on('click', '.page-next', function (e) {
+        $(".list-parent").on('click', '.page-next', function (e) {
             const pageControl = findPageControl(this);
             const pageNumber = pageControl.find('.page-number');
             pageNumber[0].stepUp();
         });
 
-        $(document).on('click', '.page-prev', function(e) {
+        $(".list-parent").on('click', '.page-prev', function(e) {
             const pageControl = findPageControl(this);
             const pageNumber = pageControl.find('.page-number');
             if (pageNumber.val() > 1) {
@@ -129,18 +118,19 @@
             }
         });
 
-        $(document).on('change', '.page-size', function (e) {
+        $(".list-parent").on('change', '.page-size', function (e) {
             const pageNumber = findPageNumber(this);
             pageNumber.value = 1;
             refreshTodos(this);
         }); 
 
-        $(document).on('change', '.collapse-checkbox', function (e) {
+        $(".list-parent").on('change', '.collapse-checkbox', function (e) {
             const listParent = findListParent(this);
-            const createTodoForm = listParent.find('.todo-creation-form');
-            createTodoForm.parent().toggleClass('hidden');
+            const targetForm = $(this).data('targetform');
+            const form = listParent.find(targetForm);
+            form.parent().toggleClass('hidden');
             const icon = $(this).parent().find('.collapse-icon');
-            icon.toggleClass('rotate-90');
+            icon.toggleClass('-rotate-90');
         });
         delete window.todoListAjaxInit;
     }
